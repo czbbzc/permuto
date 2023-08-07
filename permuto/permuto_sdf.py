1bf607335cb1ad8b5c0258642547fd627bbf4fbe
czbbzc <czbbzc7690@126.com>
2023-08-07 09:39:56 +0800

    modified:   permuto/permuto_sdf.py parameters

--- permuto/permuto_sdf.py
+++ permuto/permuto_sdf.py
@@ -66,9 +66,9 @@ class PermutoSDFModelConfig(PermutoBaseModelConfig):
     use_single_jitter: bool = True
     """Whether use single jitter or not for the proposal networks."""
     
-    curvature_weight=0.55
+    curvature_weight=0.65
     """weight of the curvature loss"""
-    lipshitz_weight=1.5e-6
+    lipshitz_weight=3e-6
     """weight of the lipshitz loss"""
     start_reduce_curv=5000
     """start to reduce the weight of the curvature loss, and add the lipshitz loss"""
